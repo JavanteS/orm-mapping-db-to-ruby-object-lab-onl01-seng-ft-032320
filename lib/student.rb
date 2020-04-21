96864dbae065fdd1900db373d192f2048ab6caf7
Javante Nelson <javantenelson@gmail.com>
2020-04-21 22:28:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -69,9 +69,8 @@ class Student
   def self.students_below_12th_grade
     #binding.pry
     sql = "SELECT * FROM students WHERE grade IS NOT 12"
-     DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end.size
+     DB[:conn].execute(sql)
+    end
   end 
   
   def self.first_X_students_in_grade_10(num_students)
